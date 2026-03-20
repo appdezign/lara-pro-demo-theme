d5c42fcbf0479446f322f8bb990f50ea28e85ffa
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-03-20 11:31:30 +0100

    Fix asset path

--- views/content/home/show.blade.php
+++ views/content/home/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('head-after')
-	<link media="all" type="text/css" rel="stylesheet" href="/assets/themes/demo/vendor/img-comparison-slider/img-comparison-slider.css">
+	{!! Theme::css('vendor/img-comparison-slider/img-comparison-slider.css') !!}
 @endsection
 
 @section('content')
@@ -55,5 +55,5 @@
 
 @section('scripts-after')
 	@parent
-	<script src="/assets/themes/demo/vendor/img-comparison-slider/img-comparison-slider.js"></script>
+	{!! Theme::js('vendor/img-comparison-slider/img-comparison-slider.js') !!}
 @endsection
